5db7a67857b242e34b5e3492954b363ba6e8ef81
ChvrlixH <ehedzade.huseyn@gmail.com>
2022-12-07 04:53:39 +0400

    Extension Contains

---
+++ Extensions/Extension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    internal static class Extension
+    {
+        public static bool customContains(this string write, string read)
+        {
+
+
+            if (read.Equals(write) || read.Equals(write.ToLower()))
+            {
+                return true;
+            }
+
+            string[] arr = write.Split();
+
+            foreach (string word in arr)
+            {
+               if (word == read)
+                {
+                    return true;
+                    break;
+                }
+                    else if (read.Equals(word))
+                    {
+                        return true;
+                    }
+                else if (read.Equals(word.ToLower()))
+                {
+                    return true;
+                }
+                else if (word.EndsWith(read))
+                {
+                    return true;
+                }
+                else if (word.StartsWith(read))
+                    {
+                        return true;
+                    }
+                
+                else if (read.Trim().EndsWith(word))
+                    {
+                        return true;
+                    }
+                else if (read.Trim().StartsWith(word))
+                {
+                    return true;
+                }
+
+            }         
+         
+            return false;
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string write = "Bu bir custom contains methodudur";
+            Console.WriteLine(write.customContains(" bir Custom co "));
+        }
+    }
+}
